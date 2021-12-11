f0ce704982e7c4439a27960cfdb3c719ca78c98f
Cheong Kah Hong <Prince@Cheongs-MacBook-Pro.local>
2021-12-12 02:13:37 +0800

    Added navbar routing and styling

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link"
+import Router, { useRouter } from "next/router";
 
 const Navbar = () => {
+    const router = useRouter();
+
     return(
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <Link href="/first">
-                <a className="mr-5 hover:text-gray-900" >First Experiment</a>
+        <nav className="">
+            <Link href="/">
+                <a className={`p-6 w-50 block hover:text-gray-900 ${router.pathname == "/" ? "bg-blue-900" : ""}`}>First Experiment</a>
             </Link>
             <Link href="/second">
-                <a className="mr-5 hover:text-gray-900">Second Experiment</a>
+                <a className="p-6 w-50 block hover:text-gray-900">Second Experiment</a>
             </Link>
             <Link href="/third">
-                <a className="mr-5 hover:text-gray-900">Third Experiment</a>
+                <a className="p-6 w-50 block hover:text-gray-900">Third Experiment</a>
             </Link>
             <Link href="/fourth">
-                <a className="mr-5 hover:text-gray-900">Fourth Experiment</a>
+                <a className="p-6 w-50 block hover:text-gray-900">Fourth Experiment</a>
             </Link>
         </nav>
     )
