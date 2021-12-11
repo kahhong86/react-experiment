d00d7a3fca349dd6c52b2b038efda547fa6f88e4
Cheong Kah Hong <Prince@Cheongs-MacBook-Pro.local>
2021-12-12 02:15:31 +0800

    Added timeout functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import Layout from '../src/components/layout'
 
+const delay = 5;
+
 const Home = () => {
+  const [show, setShow] = useState<Boolean>(false);
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(()=>{
+    let timer = setTimeout(() => {
+      if(count < 10){
+        setCount(count+1);
+        console.log(count)
+      }
+    },1000);
+  },[count]);
+  
+
   return (
     <Layout title="Experiment 1">
-      Testing 1234
+      <h1 className="text-2xl font-bold mb-5">Experiment 1 (Interval but with timeout)</h1>
+      <div>
+        {count}
+      </div>
     </Layout>
   )
 }
