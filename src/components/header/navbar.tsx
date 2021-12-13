46b19a19427c43930e5080a803c2cb47281a86a3
Cheong Kah Hong <Prince@Cheongs-MacBook-Pro.local>
2021-12-14 00:48:38 +0800

    Added pathname and router for active class page

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -10,13 +10,13 @@ const Navbar = () => {
                 <a className={`p-6 w-50 block hover:text-gray-900 ${router.pathname == "/" ? "bg-blue-900" : ""}`}>First Experiment</a>
             </Link>
             <Link href="/second">
-                <a className="p-6 w-50 block hover:text-gray-900">Second Experiment</a>
+                <a className={`p-6 w-50 block hover:text-gray-900" ${router.pathname == "/second" ? "bg-blue-900" : ""}`}>Second Experiment</a>
             </Link>
             <Link href="/third">
-                <a className="p-6 w-50 block hover:text-gray-900">Third Experiment</a>
+                <a className={`p-6 w-50 block hover:text-gray-900" ${router.pathname == "/third" ? "bg-blue-900" : ""}`}>Third Experiment</a>
             </Link>
             <Link href="/fourth">
-                <a className="p-6 w-50 block hover:text-gray-900">Fourth Experiment</a>
+                <a className={`p-6 w-50 block hover:text-gray-900" ${router.pathname == "/fourth" ? "bg-blue-900" : ""}`}>Fourth Experiment</a>
             </Link>
         </nav>
     )
