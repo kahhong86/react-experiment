e323d09020b38d537659a5d64855ff985f0c4513
Cheong Kah Hong <Prince@Cheongs-MacBook-Pro.local>
2021-12-22 17:43:55 +0800

    Added fifth page

---
+++ pages/fifth.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+import Layout from "../src/components/layout";
+
+interface FifthExperimentProps{
+}
+
+const FifthExperiment:FunctionComponent<FifthExperimentProps> = () => {
+    return(
+        <Layout title="Fifth Experiment">
+            <h1>To be Continued....</h1>
+        </Layout>
+    )
+}
+
+export default FifthExperiment;
