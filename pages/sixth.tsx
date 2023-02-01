849cc2ffe92db8135fc835864a0d74271f518d0b
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2023-02-01 09:16:40 +0800

    Cleanup sixth

--- pages/sixth.tsx
+++ pages/sixth.tsx
@@ -4,21 +4,21 @@ import Layout from "../src/components/layout";
 
 const SixthExperiment: NextPage = () => {
     const DateTime = new Date().toLocaleTimeString('en-GB');
-
-    const [allSeconds,setAllSeconds] = useState<number>(new Date().getHours()*60*60+new Date().getMinutes()*60+new Date().getSeconds());
+    const initialSeconds = new Date().getHours()*60*60+new Date().getMinutes()*60+new Date().getSeconds();
+    const [allSeconds,setAllSeconds] = useState<number>(initialSeconds);
 
     const clockHour = Math.floor(allSeconds/60/60);
     const clockMinute = Math.floor((allSeconds - clockHour*60*60)/60);
     const clockSecond = (allSeconds - (clockHour*60*60 + clockMinute*60))
 
-    const [newHour,setNewHour] = useState<number>(Math.floor(clockHour));
-    const [newMinute,setNewMinute] = useState<number>(Math.floor(clockMinute));
-    const [newSecond,setNewSecond] = useState<number>(clockSecond);
+    const [newHour,setNewHour] = useState<number>(0);
+    const [newMinute,setNewMinute] = useState<number>(0);
+    const [newSecond,setNewSecond] = useState<number>(0);
     const [changeInput, setChangeInput] = useState<number>(1);
     const [useInput,setUseInput] = useState<number>(1);
 
-    console.log("clock ", clockHour, clockMinute, clockSecond)
-    console.log("new ", newHour, newMinute, newSecond)
+    // console.log("clock ", clockHour, clockMinute, clockSecond)
+    // console.log("new ", newHour, newMinute, newSecond)
 
     useEffect(() => {
         let timerSec = setTimeout(() => {
@@ -46,7 +46,7 @@ const SixthExperiment: NextPage = () => {
     return(
         <Layout title="Sixth Experiment">
             <h1>Sixth Experiment (Backward Clock)</h1>
-            <p className="mb-5">Current Time: {DateTime}</p>
+            {/* <p className="mb-5">Current Time: {DateTime}</p> */}
             <p className="mb-5">Clock that goes backward instead of forward</p>
             <p className="inline-block w-36"><strong className="w-20 block">Hours :</strong>   <span className="text-8xl">{newHour}</span></p>
             <p className="inline-block w-36"><strong className="w-20 block">Minutes :</strong> <span className="text-8xl">{newMinute}</span></p>
