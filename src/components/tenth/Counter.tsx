5b341595cbda7899488eb550aee536be2378e1e9
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2023-03-03 20:16:16 +0800

    Updated new styling and added edit n save function

--- src/components/tenth/Counter.tsx
+++ src/components/tenth/Counter.tsx
@@ -1,12 +1,16 @@
 import { FunctionComponent, useEffect } from "react";
 import { useSelector,useDispatch } from "react-redux";
 import { useAppSelector, useAppDispatch } from './app/hooks';
-import { createList,deleteItem } from "./features/counterSlice";
+import { createList, deleteItem ,editItem, updateItem} from "./features/counterSlice";
 import sampleReducer from "./features/messageSlice";
 import { useState } from "react";
 
 interface CounterProps{
     
+}
+interface inputValueProps{
+    message:string,
+    edit:boolean,
 }
 
 const Counter:FunctionComponent<CounterProps> = () => {
@@ -15,22 +19,55 @@ const Counter:FunctionComponent<CounterProps> = () => {
     const data = useAppSelector((state) => state.counter.data);
     const dispatch = useAppDispatch();
     const [list,setList] = useState<string>("");
+    const [newInputValue, setNewInputValue] = useState<any>(({
+        message:"",
+        edit:false,
+    }));
+    const [inputValue, setInputValue] = useState<inputValueProps>({
+        message:"",
+        edit:false,
+    });
 
-    useEffect(() => {
-        console.log('msg',msg);
-        console.log('data',data);
-    },[data]);
+    const handleChange = (e) => {
+        setInputValue((prev)=> ({
+            //...prev is important, return back the previous value. without this, it will only record one set of data.
+            ...prev, 
+            message: e.target.value,
+            edit:false,
+        }));
+    }
+
+    const editChange = (value,e) => {
+        const newData = [...data];
+        const newValue = {message: value.target.value, edit: false}
+        newData[e] = newValue;
+        setNewInputValue(newData);
+        console.log(newInputValue);
+    }
 
     return(
-        <div>
-            <input className="border-black border mr-2 p-1" type="text" value={list} onChange={e => setList(e.target.value)} />
-            <button className="bg-blue-500 py-1 px-3 text-white" onClick={() => dispatch(createList(list))}>Submit</button>
-            <ul className="ml-4 mt-2">
+        <div className="mb-2 relative pb-2">
+            <div className="mb-10">
+                <input className="border-2 border-black mr-3" type="text" onChange={handleChange} />
+                <button className="bg-blue-500 py-1 px-3 text-white" onClick={() => dispatch(createList(inputValue))}>Submit</button>    
+            </div>
+            <h2 className="font-bold border-b-2 border-black mb-2">To Do List</h2>
+            <ul className="mt-2">
                 {data.map((list,index) => {
                     return(
-                        <li className="list-disc" key={index}>
-                            {list} <br />
-                            <button className="bg-red-500 p-1 text-sm text-white" onClick={() => dispatch(deleteItem(index))}>Delete</button>
+                        <li className="mb-2 relative border-b-2 pb-2" key={index}>
+                            { list.edit ? 
+                                <>
+                                    <input type="text" className="border-2 mb-2" defaultValue={list.message} onChange={(value) => editChange(value,index)}/> <br />
+                                    <button className="bg-blue-500 rounded-xl px-2 text-white w-14" onClick={() => dispatch(updateItem(newInputValue))}>Save</button>
+                                </>
+                                :
+                                <>
+                                    <span className="border-2 border-transparent relative inline-block mb-2">{list.message}</span> <br />
+                                    <button className="bg-blue-500 rounded-xl px-2 text-white w-14" onClick={() => dispatch(editItem(index))}>Edit</button>
+                                </>
+                            }
+                            <button className="bg-red-500 text-white px-2  ml-2 rounded-xl" onClick={() => dispatch(deleteItem(index))}>Delete</button>
                         </li>
                     )
                 })}
--- src/components/tenth/features/counterSlice.tsx
+++ src/components/tenth/features/counterSlice.tsx
@@ -5,7 +5,8 @@ import type { RootState } from '../app/store'
 interface CounterState {
   value: number,
   message: string,
-  data: any
+  data: any,
+  edit:boolean,
 }
 
 // Define the initial state using that type
@@ -13,6 +14,7 @@ const initialState: CounterState = {
   value: 0,
   message: "",
   data: [],
+  edit: false,
 }
 
 export const counterSlice = createSlice({
@@ -21,18 +23,23 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     // Use the PayloadAction type to declare the contents of `action.payload`
-    createList: (state, action: PayloadAction<string>) => {
+    createList: (state, action: PayloadAction<any>) => {
         state.data.push(action.payload);
     },
     deleteItem: (state, action: PayloadAction<any>) => {
-        const newData = [...state.data];
-        newData.splice(action.payload,1);
-        state.data = newData;
+        state.data.splice(action.payload,1);
+    },
+    updateItem: (state, action: PayloadAction<any>) => {
+        console.log('actionpayload',action.payload);
+        state.data = action.payload;
+    },
+    editItem: (state,action: PayloadAction<string>) => {
+        state.data[action.payload].edit = true;
     }
   },
 })
 
-export const { createList, deleteItem } = counterSlice.actions
+export const { createList, deleteItem, editItem, updateItem } = counterSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState) => state.counter.value
