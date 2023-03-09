99ead0e4c30e2afedeb4ce1e8fd4a276f20b27c9
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2023-03-09 11:30:30 +0800

    Updated redux

--- src/components/tenth/Counter.tsx
+++ src/components/tenth/Counter.tsx
@@ -1,16 +1,15 @@
 import { FunctionComponent, useEffect } from "react";
 import { useSelector,useDispatch } from "react-redux";
 import { useAppSelector, useAppDispatch } from './app/hooks';
-import { createList, deleteItem ,editItem, updateItem} from "./features/counterSlice";
+import { createList, deleteItem ,editItem, updateItem, checkItem} from "./features/counterSlice";
 import sampleReducer from "./features/messageSlice";
 import { useState } from "react";
 
-interface CounterProps{
-    
-}
+interface CounterProps{}
 interface inputValueProps{
     message:string,
     edit:boolean,
+    check:boolean,
 }
 
 const Counter:FunctionComponent<CounterProps> = () => {
@@ -19,14 +18,13 @@ const Counter:FunctionComponent<CounterProps> = () => {
     const data = useAppSelector((state) => state.counter.data);
     const dispatch = useAppDispatch();
     const [list,setList] = useState<string>("");
-    const [newInputValue, setNewInputValue] = useState<any>(({
-        message:"",
-        edit:false,
-    }));
+    const [newInputValue, setNewInputValue] = useState([]);
     const [inputValue, setInputValue] = useState<inputValueProps>({
         message:"",
-        edit:false,
+        edit: null,
+        check: null,
     });
+    const tempData = data;
 
     const handleChange = (e) => {
         setInputValue((prev)=> ({
@@ -34,16 +32,34 @@ const Counter:FunctionComponent<CounterProps> = () => {
             ...prev, 
             message: e.target.value,
             edit:false,
+            check: false,
         }));
     }
 
     const editChange = (value,e) => {
-        const newData = [...data];
-        const newValue = {message: value.target.value, edit: false}
-        newData[e] = newValue;
+        const newData:any = {message:value.target.value, index:e}
         setNewInputValue(newData);
-        console.log(newInputValue);
     }
+
+    const printList = data.map((list,index) => {
+        return(
+            <li className="mb-2 relative border-b-2 pb-2" key={index}>
+                <input type="checkbox" defaultChecked={list.check} name="check" onChange={() => dispatch(checkItem(index))}/>
+                { list.edit ? 
+                    <>
+                        <input type="text" className="border-2 mb-2" defaultValue={list.message} onChange={(value) => editChange(value,index)}/> <br />
+                        <button className="bg-blue-500 rounded-xl px-2 text-white w-14" onClick={() => dispatch(updateItem(newInputValue))}>Save</button>
+                    </>
+                    :
+                    <>
+                        <span className={`border-2 border-transparent relative inline-block mb-2 ${list.check === true ? " line-through " : "aa"}`}>{list.message}</span> <br />
+                        <button className="bg-blue-500 rounded-xl px-2 text-white w-14" onClick={() => dispatch(editItem(index))}>Edit</button>
+                    </>
+                }
+                <button className="bg-red-500 text-white px-2  ml-2 rounded-xl" onClick={() => dispatch(deleteItem(index))}>Delete</button>
+            </li>
+        )
+    })
 
     return(
         <div className="mb-2 relative pb-2">
@@ -53,24 +69,7 @@ const Counter:FunctionComponent<CounterProps> = () => {
             </div>
             <h2 className="font-bold border-b-2 border-black mb-2">To Do List</h2>
             <ul className="mt-2">
-                {data.map((list,index) => {
-                    return(
-                        <li className="mb-2 relative border-b-2 pb-2" key={index}>
-                            { list.edit ? 
-                                <>
-                                    <input type="text" className="border-2 mb-2" defaultValue={list.message} onChange={(value) => editChange(value,index)}/> <br />
-                                    <button className="bg-blue-500 rounded-xl px-2 text-white w-14" onClick={() => dispatch(updateItem(newInputValue))}>Save</button>
-                                </>
-                                :
-                                <>
-                                    <span className="border-2 border-transparent relative inline-block mb-2">{list.message}</span> <br />
-                                    <button className="bg-blue-500 rounded-xl px-2 text-white w-14" onClick={() => dispatch(editItem(index))}>Edit</button>
-                                </>
-                            }
-                            <button className="bg-red-500 text-white px-2  ml-2 rounded-xl" onClick={() => dispatch(deleteItem(index))}>Delete</button>
-                        </li>
-                    )
-                })}
+                {printList}
             </ul>
         </div>
     )
--- src/components/tenth/features/counterSlice.tsx
+++ src/components/tenth/features/counterSlice.tsx
@@ -25,21 +25,31 @@ export const counterSlice = createSlice({
     // Use the PayloadAction type to declare the contents of `action.payload`
     createList: (state, action: PayloadAction<any>) => {
         state.data.push(action.payload);
+        console.log('createlist state ',state.data[0]);
     },
     deleteItem: (state, action: PayloadAction<any>) => {
         state.data.splice(action.payload,1);
     },
     updateItem: (state, action: PayloadAction<any>) => {
-        console.log('actionpayload',action.payload);
-        state.data = action.payload;
+        const msg = action.payload.message;
+        const index = action.payload.index;
+        state.data[index].message = msg;
+        state.data[index].edit = false;
     },
     editItem: (state,action: PayloadAction<string>) => {
         state.data[action.payload].edit = true;
+    },
+    checkItem: (state,action: PayloadAction<any>) => {
+        if(state.data[action.payload].check == true){
+            state.data[action.payload].check = false;
+        }else{
+            state.data[action.payload].check = true;
+        }
     }
   },
 })
 
-export const { createList, deleteItem, editItem, updateItem } = counterSlice.actions
+export const { createList, deleteItem, editItem, updateItem, checkItem } = counterSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState) => state.counter.value
